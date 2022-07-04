78b73c79163203d66eae99985f3b44c7e6dbb415
Griillz <anthonydeangelis7877@gmail.com>
2022-07-04 11:34:15 -0400

    Fixed some styling

--- src/components/UI/Landing.js
+++ src/components/UI/Landing.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import classes from "./Landing.module.css";
-import background from "../../assets/snowblow_bg.jpg";
 
 const Landing = (props) => {
   return (
-    <div className={classes.landing}>
-      <img src={background} alt="Snowblower" className={classes.landing__img} />
-      <h1 className={classes.title}>Seasonal Snow Removal Services</h1>
+    <div className={classes.landing__container}>
+      <div className={classes.landing__content}>
+        <h1 className={classes.title}>Seasonal Snow Removal Services</h1>
+        <button onCilck="document.getElementById('quote').scrollIntoView();">Get your quote today!</button>
+      </div>
     </div>
   );
 };
--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -3,15 +3,19 @@ import classes from "./Navbar.module.css";
 
 const Navbar = (props) => {
   return (
-    <div className={classes.navbar}>
-      <h1>Testing</h1>
-      <nav>
-        <a href="#home">Home</a>
-        <a href="#faq">FAQ</a>
-        <a href="#pricing">Pricing</a>
-        <a href="#contact">Contact</a>
-      </nav>
-    </div>
+    // <div className={classes.navbar__container}>
+      <div className={classes.navbar}>
+        <div className={classes.navbar__logo}>
+          <h1>Testing</h1>
+        </div>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#faq">FAQ</a>
+          <a href="#pricing">Pricing</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    // </div>
   );
 };
 
