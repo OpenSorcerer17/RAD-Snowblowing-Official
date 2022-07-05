3f2afe0785da6a7b21cacb92b6e70a425b2f3791
Griillz <anthonydeangelis7877@gmail.com>
2022-07-05 00:02:41 -0400

    bleh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Landing from "./components/UI/Landing";
 import Navbar from "./components/UI/Navbar";
+import QuoteForm from "./components/UI/QuoteForm";
 import React, { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
       <Navbar />
-      <Landing />
+      <Landing/>
+      <QuoteForm />
+      
     </Fragment>
   );
 }
---
+++ src/components/UI/QuoteForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from './QuoteForm.module.css';
+
+const QuoteForm = props => {
+    return (<div className={classes.test}>
+        <h1>yo</h1>
+        <h1>yo</h1>
+        <h1>yo</h1>
+        <h1>yo</h1>
+        <h1>yo</h1>
+        <h1>yo</h1>
+    </div>);
+};
+
+export default QuoteForm;
