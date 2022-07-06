6c8367ef17981868e3a6c1d5ff5356ff046790c1
Griillz <anthonydeangelis7877@gmail.com>
2022-07-06 15:44:39 -0400

    I hate CSS

--- src/components/UI/QuoteForm.js
+++ src/components/UI/QuoteForm.js
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, {useState, Fragment} from "react";
 import classes from './QuoteForm.module.css';
+import QuoteFormItem from "./QuoteFormItem";
 
 const QuoteForm = props => {
     const [firstName, setFirstName] = useState('');
@@ -8,6 +9,8 @@ const QuoteForm = props => {
     const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
+    const [error, setError] = useState({error: false,
+    message: ''})
 
     const firstNameChangeHandler = (event) => {
         setFirstName(event.target.value);
@@ -35,87 +38,51 @@ const QuoteForm = props => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        if(firstName.trim().length === 0) {
-            console.log("First name cannot be blank");
-        }
 
-        if(lastName.trim().length === 0) {
-            console.log("Last name cannot be blank");
-        }
-
-        if(email.trim().length === 0) {
-            console.log("Email name cannot be blank");
-        }
-
-        if(phone.trim().length === 0) {
-            console.log("Phone name cannot be blank");
-        }
-
-        if(address.trim().length === 0) {
-            console.log("Address name cannot be blank");
-        }
-
-        if(city.trim().length === 0) {
-            console.log("City name cannot be blank");
+        if(phone.trim().length != 10) {
+            setError(prevState => ({
+                error: {
+                    ...prevState.error,
+                    message: "Please enter a valid 10 digit phone number"
+                }
+            }))
         }
     };
 
-    return (<div className={classes.quote__container} id="pricing">
-        <h1>Fill out the form below to get your quote!</h1>
-        <form>
-            <div className={classes.quote__container__row}>
-            <label>
-                First Name
-                <input type="text" name="first_name"  onChange={firstNameChangeHandler} />
-            </label>
-            <label>
-                Last Name
-                <input type="text" name="last_name" onChange={lastNameChangeHandler} />
-            </label>
-            </div>
-            <div className={classes.quote__container__row}>
-            <label>
-                Email Address
-                <input type="email" name="email" onChange={emailChangeHandler} />
-            </label>
-            <label>
-                Phone Number
-                <input type="text" name="phone" onChange={phoneChangeHandler} />
-            </label>
-            </div>
-            <div className={classes.quote__container__row}>
-            <label>
-                Street Address
-                <input type="text" name="address" onChange={addressChangeHandler} />
-            </label>
-            <label>
-                City
-                <input type="text" name="city" onChange={cityChangeHandler} />
-            </label>
-            </div>
-            <div className={classes.quote__container__checkbox}>
-            <input type="checkbox"/>
-                <label>I have a paved driveway
-                    
-                </label>
-                <input type="checkbox"/>
-                <label>I have a concrete driveway
-                </label>
-                <input type="checkbox"/>
-                <label>I am interested in de-icing service
-                </label>
-                <input type="checkbox"/>
-                <label>I am interested in sidewalk snow-shoveling
-                </label>
-            </div>
-            <div>
-                <p>Include any additional information that might help us </p>
-                <textarea></textarea>
-            </div>
-            <button onClick={onSubmitHandler}>Submit</button>
+    return (<Fragment><h1 className={classes.quote__container}>Fill out the form below to get your quote!</h1>
+    <div className={classes.quote__container} id="pricing">
+    <form onSubmit={onSubmitHandler}>
+        <div className={classes.quote__container__row}>
+        <QuoteFormItem  changeHandler={firstNameChangeHandler} content="First name" inputType="text" inputName="first_name"/>
+        <QuoteFormItem  changeHandler={lastNameChangeHandler} content="Last name" inputType="text" inputName="last_name"/>
+        </div>
+        <div className={classes.quote__container__row}>
+        <QuoteFormItem  changeHandler={emailChangeHandler} content="Email" inputType="email" inputName="email"/>
+        <QuoteFormItem  changeHandler={phoneChangeHandler} content="Phone Number" inputType="tel" inputName="phone"/>
+        </div>
+        <div className={classes.quote__container__row}>
+        <QuoteFormItem  changeHandler={addressChangeHandler} content="Street Address" inputType="text" inputName="address"/>
+        <QuoteFormItem  changeHandler={cityChangeHandler} content="City" inputType="text" inputName="city"/>
+        </div>
+        
+        <div className={classes.quote__container__checkbox}>
+            <QuoteFormItem changeHandler='' content="I want de-icing services" inputType="checkbox"/>
+            <QuoteFormItem changeHandler='' content="I want my walkway shoveled" inputType="checkbox"/>
+            <QuoteFormItem changeHandler='' content="Test 3" inputType="checkbox"/>
+            <QuoteFormItem changeHandler='' content="Test 4" inputType="checkbox"/>
             
-        </form>
-    </div>);
-};
-
+        </div>
+        
+        <div className={classes.lower__styling}>
+            <p>Include any additional information that might help us </p>
+            <textarea></textarea>
+        </div>
+        <button type="submit">Submit</button>
+        
+        
+    </form >
+    </div></Fragment>
+        
+    );
+}
 export default QuoteForm;
---
+++ src/components/UI/QuoteFormItem.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from "react";
+
+const QuoteFormItem = props => {
+    return (
+    <Fragment>
+    {props.inputType==="checkbox" &&
+    <label>
+    <input type={props.inputType} name={props.inputName} required  onChange={props.changeHandler} />
+    {props.content}
+    </label>}
+    {props.inputType!="checkbox" && <label>
+        {props.content}
+        <input type={props.inputType} name={props.inputName} required  onChange={props.changeHandler} />
+    </label>}
+    </Fragment>)
+
+};
+
+export default QuoteFormItem;
